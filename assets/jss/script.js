5827e20f68e685e0e2b3a45a901af1401791ce69
Skwiera <mckskwiera@gmail.com>
2023-01-19 16:02:23 +0000

    day plan fields rendering properly

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -14,12 +14,18 @@ let hours = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:0
 
 // for loop going through hours array and appending list item to list for each
 for (let i = 0; i < hours.length; i++) {
+    //creating list item
     let listItem = $('<li>')
     listItem.addClass('row time-block')
+    // creating hour block and appending to list item
     let hour = $('<div>')
     hour.addClass('hour col-1')
     hour.text(hours[i])
     listItem.append(hour)
+    // creating day plan area and appending to list item
+    let dayPlan = $('<div>')
+    dayPlan.addClass('textarea col-10')
+    listItem.append(dayPlan)
     for (let j = 0; j < hours.length; j++) {
         list.append(listItem)
     }
