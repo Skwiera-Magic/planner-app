cd347cbec02a1ac5d64cc5dfe076dabd149a20bb
Skwiera <mckskwiera@gmail.com>
2023-01-19 15:12:45 +0000

    list of hours renders properly

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -4,14 +4,20 @@ let log = console.log.bind(document)
 $("#currentDay").text(moment().format("h:mm:ssa, dddd, Do MMM YYYY"))
 
 
+// array of buisness hours
+let hours = ["9", "10", "11", "12", "13", "14", "15", "16"]
+
 // rendering list of hour blocks
-let container = $('.container')
+for (let i = 0; i < hours.length; i++) {
+    let container = $('.container')
 let list = $('<ul>')
 container.append(list)
 let listItem = $('<li>')
-listItem.text('test')
+listItem.text(hours[i])
 listItem.attr("class", "row")
 //listItem.attr("class", "time-block")
 for (let i = 0; i < 25; i++) {
 list.append(listItem[i])
+}
+
 }
