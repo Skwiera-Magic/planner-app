d07b4f6ddabffbeec165dbbd12d932e74263ca37
Skwiera <mckskwiera@gmail.com>
2023-01-19 15:36:27 +0000

    one list created instead of 8 separate ones

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -5,19 +5,22 @@ $("#currentDay").text(moment().format("h:mm:ssa, dddd, Do MMM YYYY"))
 
 
 // array of buisness hours
-let hours = ["9", "10", "11", "12", "13", "14", "15", "16"]
+let hours = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"]
 
 // rendering list of hour blocks
-for (let i = 0; i < hours.length; i++) {
     let container = $('.container')
-let list = $('<ul>')
-container.append(list)
-let listItem = $('<li>')
-listItem.text(hours[i])
-listItem.attr("class", "row")
-//listItem.attr("class", "time-block")
-for (let i = 0; i < 25; i++) {
-list.append(listItem[i])
-}
-
+    let list = $('<ul>')
+    container.append(list)
+for (let i = 0; i < hours.length; i++) {
+    let listItem = $('<li>')
+    listItem.text(hours[i])
+    listItem.attr('class', 'row')
+    let hourBlock = $('<span>')
+    hourBlock.attr('class', 'time-block')
+    hourBlock.css('background-color', 'blue')
+    listItem.append(hourBlock)
+    // todo: i'm not sure why this loop is here and why it's not working without it??
+    for (let i = 0; i < 10; i++) {
+        list.append(listItem[i])
+    }
 }
